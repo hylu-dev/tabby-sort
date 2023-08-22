cd1f2b0ecf59d6b8e2a79b37cd82e84c3bc65881
hylu-dev <roylu99@gmail.com>
2023-08-22 08:02:18 -0400

    Local storage checkboxes and group sorting

--- popup.js
+++ popup.js
@@ -1,13 +1,35 @@
 let sortCtrl;
 let labelCtrl;
 let hideCtrl;
-document.getElementById("button-group").addEventListener('click', sortTabs);
-document.getElementById("button-ungroup").addEventListener('click', ungroupTabs);
+let colors = ["pink","purple","cyan","orange","blue","red","yellow","green","grey"];
+
 document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("button-group").addEventListener('click', sortTabs);
+    document.getElementById("button-ungroup").addEventListener('click', ungroupTabs);
+    document.getElementById("sort").addEventListener('change', storeInputValue);
+    document.getElementById("label").addEventListener('change', storeInputValue);
+    document.getElementById("hide").addEventListener('change', storeInputValue);
+
     sortCtrl = document.getElementById("sort");
     labelCtrl = document.getElementById("label");
     hideCtrl = document.getElementById("hide");
+    loadInputValue(sortCtrl);
+    loadInputValue(labelCtrl);
+    loadInputValue(hideCtrl);
 });
+
+function loadInputValue(ctrl) {
+    chrome.storage.local.get(
+        [ctrl.id]
+    ).then( result => ctrl.checked = result[[ctrl.id]])
+}
+
+function storeInputValue(e) {
+    payload = {}
+    payload[e.target.id] = e.target.checked;
+    chrome.storage.local.set(payload);
+    
+}
 
 function sortTabs() {
     if (sortCtrl.checked) {
@@ -25,30 +47,49 @@ function groupTabs(tabs) {
     const domainTabIds = {};
     let groupDomains = [];
 
+    // retrieve unique domains from tabs
     for (let tab of tabs) {
         let domain = getDomainFromURL(tab.url);
         domainTabIds[domain] || (domainTabIds[domain] = []);
         domainTabIds[domain].push(tab.id);
     }
+
+    // group by domains
+    let i = 0
     for (let domain in domainTabIds) {
         if (domainTabIds[domain].length > threshold) { 
             chrome.tabs.group({
                 tabIds: domainTabIds[domain]
             })
-            groupDomains.push(domain);
+            groupDomains.push([i, domain]);
+            i++;
         }
     }
 
+    const sortedGroupDomains = groupDomains.sort(
+        (a, b) => a[1] < b[1] ? 1 : -1
+    );
 
+    // collapse/title groups
     chrome.tabGroups.query({}, groups => {
-        for (let index in groups) {
-            chrome.tabGroups.update(groups[index].id,
+        for (let indexDomainPair of sortedGroupDomains) {
+            chrome.tabGroups.update(groups[indexDomainPair[0]].id,
             {
                 collapsed: hideCtrl.checked,
-                title: labelCtrl.checked ? groupDomains[index] : ''
+                title: labelCtrl.checked ? indexDomainPair[1] : '',
+                color: colors[0]
+            })
+            colors.push(colors.shift());
+            // move to end
+            if (sortCtrl.checked) chrome.tabGroups.move(groups[indexDomainPair[0]].id, {
+                index: -1
             })
         }
     })
+}
+
+function sortGroups(e) {
+    console.log(e);
 }
 
 function ungroupTabs() {
